f681cd8b5c0077a7ab96401900d758f32c93ce94
曾学锴 <781309842@qq.com>
2021-01-06 14:22:13 +0800

    V1.0更新

---
+++ src/main/java/cn/zxk/mappers/TFeedbackMapper.java
@@ -0,0 +1,17 @@
+package cn.zxk.mappers;
+
+import cn.zxk.entity.TFeedback;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Chick
+ * @since 2021-01-06
+ */
+public interface TFeedbackMapper extends BaseMapper<TFeedback> {
+
+}
---
+++ src/main/java/cn/zxk/mappers/TResSortMapper.java
@@ -0,0 +1,17 @@
+package cn.zxk.mappers;
+
+import cn.zxk.entity.TResSort;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Chick
+ * @since 2021-01-06
+ */
+public interface TResSortMapper extends BaseMapper<TResSort> {
+
+}
